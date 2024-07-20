284eb29712107d6ee4866055a448d9fe24a103c6
Lake Biwa <stickfigure.work@gmail.com>
2024-07-20 22:56:44 +0900

    Add files via upload

---
+++ StdOut.java
@@ -0,0 +1,9 @@
+public final class StdOut{
+    public final static String RED    = "\u001b[00;31m";
+    public final static String GREEN  = "\u001b[00;32m";
+    public final static String YELLOW = "\u001b[00;33m";
+    public final static String PURPLE = "\u001b[00;34m";
+    public final static String PINK   = "\u001b[00;35m";
+    public final static String CYAN   = "\u001b[00;36m";   
+    public final static String END    = "\u001b[00m";
+}
